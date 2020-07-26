c6db93d7d5fc3148e7f3ace6962d6bacb2ce6119
hannah_kwakye <hannah.kwakye@makeduconsult.com>
2020-07-26 16:33:39 -0400

    Added shop files in the site folder

---
+++ public/js/pages/roles.js
@@ -0,0 +1,15 @@
+function deleteRole(id) {
+    swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!'
+    }).then(function (result) {
+        if (result.value) {
+            $("#delete_form_"+id).submit();
+        }
+    });
+}
+
+
