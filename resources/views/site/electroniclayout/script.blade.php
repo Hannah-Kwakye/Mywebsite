ebd4e7e048945fdd725d6c07fe69221bdf482b4d
hannah_kwakye <hannah.kwakye@makeduconsult.com>
2020-07-26 16:26:27 -0400

    Added Multi-store layout

---
+++ resources/views/site/electroniclayout/script.blade.php
@@ -0,0 +1,42 @@
+<script src="{{asset('js/vendor/modernizr-2.8.3.min.js')}}"></script> 
+<!-- begin::Global Config(global config for global JS sciprts) -->
+<script>
+   var KTAppOptions = {
+       "colors": {
+           "state": {
+               "brand": "#5d78ff",
+               "dark": "#282a3c",
+               "light": "#ffffff",
+               "primary": "#5867dd",
+               "success": "#34bfa3",
+               "info": "#36a3f7",
+               "warning": "#ffb822",
+               "danger": "#fd3995"
+           },
+           "base": {
+               "label": ["#c5cbe3", "#a1a8c3", "#3d4465", "#3e4466"],
+               "shape": ["#f0f3ff", "#d9dffa", "#afb4d4", "#646c9a"]
+           }
+       }
+   };
+</script>
+<!-- end::Global Config -->
+<!--begin::Global Theme Bundle(used by all pages) -->
+<script src="{{asset('vendors/global/vendors.bundle.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/scripts.bundle.js')}}" type="text/javascript"></script>
+<!--end::Global Theme Bundle -->
+<!--begin::Page Scripts(used by this page) -->
+<script src="{{asset('js/pages/login-1.js')}}" type="text/javascript"></script>
+   <!-- all js here -->
+   <script src="{{asset('js/vendor/jquery-1.12.0.min.js')}}"></script>
+   <script src="{{asset('js/popper.js')}}"></script>
+   <script src="{{asset('js/bootstrap.min.js')}}"></script>
+   <script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+   <script src="{{asset('js/isotope.pkgd.min.js')}}"></script>
+   <script src="{{asset('js/imagesloaded.pkgd.min.js')}}"></script>
+   <script src="{{asset('js/jquery.counterup.min.js')}}"></script>
+   <script src="{{asset('js/waypoints.min.js')}}"></script>
+   <script src="{{asset('js/ajax-mail.js')}}"></script>
+   <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+   <script src="{{asset('js/plugins.js')}}"></script>
+   <script src="{{asset('js/main.js')}}"></script>
---
+++ resources/views/site/electroniclayout/style.blade.php
@@ -0,0 +1,34 @@
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('img/favicon.png')}}">
+ 
+    <!-- all css here -->
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('css/animate.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('css/pe-icon-7-stroke.css')}}">
+    <link rel="stylesheet" href="{{asset('css/meanmenu.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bundle.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
+    <link href="{{asset('css/pages/login/login-1.css')}}" rel="stylesheet" type="text/css" />
+ 
+        <!--end::Page Custom Styles -->
+        <!--begin::Global Theme Styles(used by all pages) -->
+        <link href="{{asset('vendors/global/vendors.bundle.css')}}"  rel="stylesheet" type="text/css" />
+        <link href="{{asset('css/style.bundle.css')}}"  rel="stylesheet" type="text/css" />
+ 
+        <!--end::Global Theme Styles -->
+ 
+        <!--begin::Layout Skins(used by all pages) -->
+        <link href="{{asset('css/skins/header/base/light.css ')}}"  rel="stylesheet" type="text/css" />
+        <link href="{{asset('css/skins/header/menu/light.css')}}"  rel="stylesheet" type="text/css" />
+        <link href="{{asset('css/skins/brand/dark.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('css/skins/aside/dark.css')}}" rel="stylesheet" type="text/css" />
+
+ 
+        <!--end::Layout Skins -->
+        <link rel="shortcut icon" href="{{asset('media/logos/favicon.ico')}}" />
+   
